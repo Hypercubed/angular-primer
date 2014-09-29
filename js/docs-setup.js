a4ad2c8847b8bfa8f9e2850d6de70cd23a196564
J. Harshbarger <hypercubed@gmail.com>
2014-09-29 15:10:52 +0900

    Updates

--- js/docs-setup.js
+++ js/docs-setup.js
@@ -1,6 +1,7 @@
 NG_DOCS={
   "sections": {
-    "api": "API Documentation"
+    "api": "API Documentation",
+    "examples": "Examples"
   },
   "pages": [
     {
@@ -10,7 +11,7 @@ NG_DOCS={
       "type": "overview",
       "moduleName": "index",
       "shortDescription": "AngularJs module for creating an interactive SVG primer visualization. This library currently depends on d3 and requires angularjs 1.3.0-rc or higher.",
-      "keywords": "add additional alignment-baseline anchor angualrjs angular angular-primer angularjs angularprimer angularprimer-example api applications array arrow-left arrow-right attributes based behavior binding black bottom built ccctgtggagccacaccctcaccct class color complex composable consists controller create creates creating crispedges css currently data depends designed direction directive directives dna domain easily element elements example examplea exampleb existing extend fc0 feature feature-track features fff fill filter float form forward function genomic getshape green grey height higher html input interactive introduction join js label left length library lightblue main maincontroller marker max middle min module ng-controller ng-model ng-repeat ng-style number org orient originally outer-tick-size overview primer primer-feature primer-feature-shape primer-label primer-scale primer-track primers rect red requires return reverse second sequence set shape-rendering simple start start-1 strand stroke stroke-width style styling svg table text text-anchor tick ticks top track transform translate turn type var view visualization visualizations visualize visulaization width yellowgreen"
+      "keywords": "add additional alignment-baseline anchor angualrjs angular angular-primer angularjs angularprimer angularprimer-example api applications array arrow-left arrow-right attributes based behavior binding black bottom built ccctgtggagccacaccctcaccct class color complex composable consists controller create creates creating crispedges css currently data depends designed direction directive directives dna domain easily element elements example examplea exampleb existing extend fc0 feature feature-track features fill filter float form forward function genomic getshape green grey height higher html input interactive introduction join js label left length library lightblue main maincontroller marker max middle min module ng-controller ng-model ng-repeat ng-style number org orient originally outer-tick-size overview primer primer-feature primer-feature-shape primer-label primer-scale primer-track primers rect red requires return reverse second sequence set shape-rendering simple start start-1 strand stroke stroke-width style styling svg table text text-anchor tick ticks top track transform translate turn type var view visualization visualizations visualize visulaization width yellowgreen"
     },
     {
       "section": "api",
@@ -28,7 +29,7 @@ NG_DOCS={
       "type": "directive",
       "moduleName": "angularprimer",
       "shortDescription": "Creates a feature on a track",
-      "keywords": "alignment-baseline anchor angular angularprimer angularprimer-example api array black bottom ccctgtggagccacaccctcaccct class color cols controller creates crispedges css direction directive domain ea exampleb fc0 feature feature-track features fill float form function green grey height html input join js label left length lightblue main maincontroller marker max middle min module ng-controller ng-model ng-repeat ng-style null number orient outer-tick-size position primer-feature primer-feature-shape primer-label primer-scale primer-track red rows sequence shape-rendering start starting stroke stroke-width style svg table text-anchor tick ticks top track transform translate type var width yellowgreen"
+      "keywords": "anchor angular angularprimer angularprimer-example api array black bottom ccctgtggagccacaccctcaccct class color cols controller creates crispedges css direction directive domain ea exampleb fc0 feature feature-track features fill float form function green grey height html input join js label left length lightblue main maincontroller marker max min module ng-controller ng-model ng-repeat ng-style null number orient outer-tick-size position primer-feature primer-feature-shape primer-label primer-scale primer-track red rows sequence shape-rendering start starting stroke stroke-width style svg table tick ticks top track transform translate type var width yellowgreen"
     },
     {
       "section": "api",
@@ -37,7 +38,7 @@ NG_DOCS={
       "type": "directive",
       "moduleName": "angularprimer",
       "shortDescription": "Creates a shape in a feature",
-      "keywords": "alignment-baseline anchor angular angularprimer angularprimer-example api arrow-left arrow-right black bottom circle class controller creates crispedges cross css defaults diamond direction directive domain ea feature feature-track features fill float form function height hieght html input js label left main maincontroller marker middle min module ng-controller ng-model ng-repeat null number option orient outer-tick-size path primer-feature primer-feature-shape primer-label primer-scale primer-track rect sequence-length shape shape-rendering square start stroke stroke-width style svg table text-anchor tick ticks top track transform translate triangle-down triangle-up type var width"
+      "keywords": "anchor angular angularprimer angularprimer-example api arrow-left arrow-right black bottom circle class controller creates crispedges cross css defaults diamond direction directive domain ea feature feature-track features fill float form function height hieght html input js label left main maincontroller marker min module ng-controller ng-model ng-repeat null number option orient outer-tick-size path primer-feature primer-feature-shape primer-label primer-scale primer-track rect sequence-length shape shape-rendering square start stroke stroke-width style svg table tick ticks top track transform translate triangle-down triangle-up type var width"
     },
     {
       "section": "api",
@@ -46,7 +47,7 @@ NG_DOCS={
       "type": "directive",
       "moduleName": "angularprimer",
       "shortDescription": "Adds a label to a feature or a track.",
-      "keywords": "adds alignment-baseline anchor angularprimer api black bottom class crispedges css directive domain ea feature fff fill height hrizontal html label marker middle module ng-init ng-model orient orientation primer-feature primer-feature-shape primer-label primer-scale primer-track sequence-length shape-rendering start stroke stroke-width svg text text-anchor tick top track transform translate type vertical width"
+      "keywords": "adds anchor angularprimer api black bottom class crispedges css directive domain ea feature fff fill height hrizontal html label marker middle module ng-init ng-model orient orientation primer-feature primer-feature-shape primer-label primer-scale primer-track sequence-length shape-rendering start stroke stroke-width svg text tick top track transform translate type vertical width"
     },
     {
       "section": "api",
@@ -55,7 +56,7 @@ NG_DOCS={
       "type": "directive",
       "moduleName": "angularprimer",
       "shortDescription": "Adds a axis to a track.",
-      "keywords": "adds alignment-baseline anchor angularprimer api approximate axis black bottom class crispedges css directive display domain ea fff fill format html inner labels marker markers middle module ng-init ng-model nuber orient orientation outer padding pixels primer-feature primer-label primer-scale primer-track scale sequence-length shape-rendering size start stroke stroke-width svg text-anchor tick top track transform translate width"
+      "keywords": "adds anchor angularprimer api approximate axis black bottom class crispedges css directive display domain ea fff fill format html inner labels marker markers middle module ng-init ng-model nuber orient orientation outer padding pixels primer-feature primer-label primer-scale primer-track scale sequence-length shape-rendering size start stroke stroke-width svg tick top track transform translate width"
     },
     {
       "section": "api",
@@ -64,11 +65,21 @@ NG_DOCS={
       "type": "directive",
       "moduleName": "angularprimer",
       "shortDescription": "Creates a container used to group features, scales, and labels.",
-      "keywords": "alignment-baseline anchor angularprimer api black class container creates crispedges css directive domain ea features fff fill group height html labels length marker middle min module ng-init ng-model number orient pixels primer-feature primer-label primer-scale primer-track scale scales sequence sequence-length shape-rendering start starting stroke stroke-width svg text-anchor tick track transform translate type width"
+      "keywords": "anchor angularprimer api black class container creates crispedges css directive domain ea features fff fill group height html labels length marker middle min module ng-init ng-model number orient pixels primer-feature primer-label primer-scale primer-track scale scales sequence sequence-length shape-rendering start starting stroke stroke-width svg tick track transform translate type width"
+    },
+    {
+      "section": "examples",
+      "id": "index",
+      "shortName": "examples",
+      "type": "overview",
+      "moduleName": "examples",
+      "shortDescription": "Additional Examples",
+      "keywords": "additional alignment-baseline anchor angularprimer arrow-left arrow-right black blue bottom circle class crispedges css dna domain examplea exampleb examplec exampled examples fill flanking font-size format green height html marker middle module orient outer-tick-size overview parental primer primer-feature primer-feature-shape primer-label primer-scale primer-track primers red rna rotate sequence-length shape-rendering start strand stroke stroke-width svg symbol template text text-anchor tick ticks top transform translate triangle-down untranslated width"
     }
   ],
   "apis": {
-    "api": true
+    "api": true,
+    "examples": false
   },
   "html5Mode": false,
   "editExample": true,
